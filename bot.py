f5e54c087d8cd37de1cd1e143c9fb1b53e15e41a
renanjp2017 <69819690+renanjp2017@users.noreply.github.com>
2026-05-28 22:24:21 -0300

    Atualizar o bot.py

--- bot.py
+++ bot.py
@@ -257,9 +257,15 @@ INVIDIOUS = [
 
 YTDL_OPTS = {
     "format": "bestaudio/best",
-    "quiet": True, "no_warnings": True,
-    "nocheckcertificate": True, "ignoreerrors": True,
+    "quiet": True,
+    "no_warnings": True,
+    "nocheckcertificate": True,
+    "ignoreerrors": True,
+    "headers": {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+    }
 }
+
 
 FFMPEG_OPTS = {
     "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
